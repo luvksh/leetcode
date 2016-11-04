135c27b62a612882906c5b92693232e907f08e24
Rahul Kumar <rahul.kmr@gmail.com>
2016-11-04 01:18:46 -0700

    update to queueReconstruction

--- queueReconstruction.cpp
+++ queueReconstruction.cpp
@@ -1,52 +1,37 @@
 //https://leetcode.com/problems/queue-reconstruction-by-height/
+struct comparator
+{
+	bool operator()(pair<int, int> a, pair<int, int> b)
+	{
+		if (a.first == b.first) return a.second > b.second;
+		else return a.first < b.first;
+	}
+}mycomparator;
 class Solution {
 public:
-    vector<pair<int, int>> reconstructQueue(vector<pair<int, int>>& people) {
-        vector<pair<pair<int,int>, int>> peopleAug;
-        for(auto p:people)
-        {
-            pair<pair<int,int>,int> pp = make_pair(p, p.second);
-            peopleAug.push_back(pp);
-        }
-        
-        vector<pair<int, int>> res;
-        pair<int,int> pa = _helper(peopleAug);
-        while(pa.second > -1)
-        {
-            res.push_back(pa);
-            pa = _helper(peopleAug);
-        }
-        return res;
-    }
-    
-    pair<int,int> _helper(vector<pair<pair<int,int>, int>> &peopleAug)
-    {
-        pair<int,int> res = make_pair(INT_MAX,-1);
-        if(peopleAug.size() == 0)
-            return res;
-        for(auto pp:peopleAug)
-        {
-            if(pp.second == 0)
-            {
-                if(pp.first.first < res.first)
-                    res = pp.first;
-            }
-        }
-        
-        int s = peopleAug.size();
-        
-        for(int i = 0; i < s; i++)
-        {
-            if(res == peopleAug[i].first && i != s-1)
-            {
-                peopleAug[i] = peopleAug[s-1];
-            }
-            
-            if(res.first >= peopleAug[i].first.first )
-                peopleAug[i].second--;
-        }
-        
-        peopleAug.pop_back();
-        return res;
-    }
+	vector<pair<int, int>> reconstructQueue(vector<pair<int, int>>& people) {
+		sort(people.begin(), people.end(), mycomparator);
+		vector<pair<int, int>> temp(people.begin(), people.end());
+		vector<pair<int, int>> result;
+		vector<bool> flag(people.size(), false);
+		for (int i = 0; i < people.size(); i++)
+		{
+			for (int j = 0; j < people.size(); j++)
+			{
+				if (flag[j] == true)
+					continue;
+				if (temp[j].second == 0)
+				{
+					result.push_back(people[j]);
+					flag[j] = true;
+					break;
+				}
+				else
+				{
+					temp[j].second--;
+				}
+			}
+		}
+		return result;
+	}
 };
