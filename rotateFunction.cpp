b25e1616c918c05961f0ad61c25e362b7e08213d
Rahul Kumar <rahul.kmr@gmail.com>
2016-11-05 13:59:59 -0700

    rotateFunction

--- rotateFunction.cpp
+++ rotateFunction.cpp
@@ -2,30 +2,24 @@ https://leetcode.com/problems/rotate-function/
 class Solution {
 public:
     int maxRotateFunction(vector<int>& A) {
-        int n = A.size();
-        if(n == 0) return 0;
-        int max = INT_MIN;
-        int totalSum = 0;
-        int prevf = 0;
-        int lastNum;
-        for(int i = 0; i < n; i++)
+        int total = 0;
+        int fprev = 0;
+        for(int i = 0; i < A.size(); i++)
         {
-            totalSum += A[i];
-            prevf += i*A[i];
+            total += A[i];
+            fprev += i*A[i];
         }
         
-        lastNum = A[n-1];
-        max = prevf;
-        
-        for(int i = 1; i < n; i++)
+        int sum = 0;
+        int max = fprev;
+        for(int i = 0; i < A.size(); i++)
         {
-            int sum = prevf;
-            sum -= (lastNum *(n-1));
-            sum += (totalSum - lastNum);
-            lastNum = A[n-i-1];
+            sum = fprev;
+            sum -= (A.size()-1) * A[A.size()-1-i];
+            sum += (total - A[A.size()-1-i]);
             if(sum > max)
                 max = sum;
-            prevf = sum;
+            fprev = sum;
         }
         return max;
     }
