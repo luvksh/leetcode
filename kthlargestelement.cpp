4772495a5308a2106ab27c70ecacf1ba3bd69a4b
Rahul Kumar <rahul.kmr@gmail.com>
2016-11-17 19:29:56 -0800

    update to kthlargestelement

--- kthlargestelement.cpp
+++ kthlargestelement.cpp
@@ -1,30 +1,35 @@
 class Solution {
+    int partition(vector<int>& nums, int l, int r)
+    {
+        int i = l-1;
+        for(int j = l; j < r; j++)
+        {
+            if(nums[j] > nums[r])
+            {
+                i++;
+                swap(nums[j], nums[i]);
+            }
+        }
+        swap(nums[r], nums[i+1]);
+        return i+1;
+    }
+    int randomized_partition(vector<int>& nums, int l, int r)
+    {
+        int a = rand()%(r-l+1) + l;
+        swap(nums[a], nums[r]);
+        return partition(nums, l, r);
+    }
     int _findKthLargest(vector<int>& nums, int l, int r, int k)
     {
-        if(r == l)
-            return nums[r];
-        int i = l;
-        int j = r - 1;
-
-        while(1)
-        {
-            while(i < r && nums[i] > nums[r])
-                i++;
-
-            while(j >= l && nums[j] <= nums[r])
-                j--;
-            if(i > j)
-                break;
-            swap(nums[i], nums[j]);
-        }
-        j++;
-        swap(nums[r], nums[j]);
-        if(j+1-l == k)
-            return nums[j];
-        else if(k < j+1-l)
-            return  _findKthLargest(nums, l, j-1, k);
+        if(l==r)
+            return nums[l];
+        int p = randomized_partition(nums, l, r);
+        if(k == p-l+1)
+            return nums[p];
+        else if(k < p-l+1)
+            return _findKthLargest(nums, l, p-1, k);
         else
-            return _findKthLargest(nums, j+1, r, k-(j+1-l));
+            return _findKthLargest(nums, p+1, r, k-(p+1-l));
     }
 public:
     int findKthLargest(vector<int>& nums, int k) {
